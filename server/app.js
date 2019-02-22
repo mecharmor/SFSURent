351d3cdea91bf7a759125a12a8a12a6c7c39b42e
sohe1l <soheilansari@icloud.com>
2019-02-21 21:04:29 -0800

    Installed mysql and setup test connection

--- server/app.js
+++ server/app.js
@@ -1,26 +1,31 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+
+const app = express();
 app.set('view engine', 'ejs');
 
 /* Debug is used for printing message on cosole in different categories.
-Run "DEBUG=* node app.js" to get all message or run "DEBUG=app node app.js" to get messages for this file only. */
-var debug = require('debug')('app');
+Run "DEBUG=* node app.js" to get all message or run "DEBUG=app node app.js"
+to get messages for this file only. */
+const debug = require('debug')('app');
 
 /* Morgan is used for logging http request in the console */
-var morgan = require('morgan');
-app.use(morgan('tiny'))
+const morgan = require('morgan');
 
-/*allows to call static items in pulic folder such as images*/
+app.use(morgan('tiny'));
+
+/* allows to call static items in pulic folder such as images */
 app.use('/public', express.static(__dirname + "/public"));
 
 const aboutRouter = require('./routes/aboutRoutes');
-app.use('/about/', aboutRouter);
+const mysqlRouter = require('./routes/mysqlRoutes');
 
-var router = express.Router();
-app.get('/', (req,res) => {
+app.use('/about/', aboutRouter);
+app.use('/mysql/', mysqlRouter);
+
+app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   debug('listening on port 3000');
 });
