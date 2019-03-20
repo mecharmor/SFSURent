d729c1d05d473a3ef30165aef343503d0b052f17
sohe1l <soheilansari@icloud.com>
2019-03-19 23:40:07 -0700

    updated credentialsls

--- src/routes/mysqlRoutes.js
+++ src/routes/mysqlRoutes.js
@@ -2,15 +2,82 @@ var express = require('express');
 var mysqlRouter = express.Router();
 const mysql = require('mysql');
 
-mysqlRouter.route('/')
-  .get((req, res) => {
-    const connection = mysql.createConnection({
-      host: 'team11-db-instance.chozbodasabv.us-west-1.rds.amazonaws.com',
-      user: 'team11master',
-      password: 'csc648team11',
+
+
+    // const connection = mysql.createConnection({
+    //   host: 'team11-db-instance.chozbodasabv.us-west-1.rds.amazonaws.com',
+    //   user: 'team11master',
+    //   password: 'csc648team11',
+    //   port: 3306,
+    //   database: 'team11db',
+    // });
+
+
+
+const mysql_config = {
+      host: 'localhost',
+      user: 'team11',
+      password: 'csc648Team11@',
       port: 3306,
       database: 'team11db',
-    });
+    };
+
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
+
+
+
+
+mysqlRouter.route('/refresh')
+  .get((req, res) => {
+
+    database = new Database(mysql_config);
+
+    database.query( 'SELECT * FROM some_table' )
+      .then( rows => database.query( 'SELECT * FROM other_table' ) )
+      .then( rows => database.close() );
+
+
+  });
+
+
+
+
+
+mysqlRouter.route('/test')
+  .get((req, res) => {
+
+    // const connection = mysql.createConnection({
+    //   host: 'team11-db-instance.chozbodasabv.us-west-1.rds.amazonaws.com',
+    //   user: 'team11master',
+    //   password: 'csc648team11',
+    //   port: 3306,
+    //   database: 'team11db',
+    // });
+
+    const connection = mysql.createConnection();
+
 
     connection.connect(function (err) {
       if (err) {
@@ -38,4 +105,10 @@ mysqlRouter.route('/')
       });
     });
   });
+
+
+
+
+
+
 module.exports = mysqlRouter;
