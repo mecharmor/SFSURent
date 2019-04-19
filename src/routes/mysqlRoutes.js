ab2d986425a5d61ecc9e4206495f026d15302573
Poorva <poorvarathi0208@gmail.com>
2019-04-18 19:34:37 -0700

    inserted values into users and features table. Connected listings with users.

--- src/routes/mysqlRoutes.js
+++ src/routes/mysqlRoutes.js
@@ -114,9 +114,9 @@ mysqlRouter.route('/refresh')
       'size int,' +
       'score int,' +
       'listing_type_id int NOT NULL,' +
-      //'user_id INT NOT NULL,' +
-      'FOREIGN KEY (listing_type_id) REFERENCES listing_type(id)'+ 
-      //'FOREIGN KEY (user_id) REFERENCES users(id)' +
+      'user_id INT NOT NULL,' +
+      'FOREIGN KEY (listing_type_id) REFERENCES listing_type(id),'+ 
+      'FOREIGN KEY (user_id) REFERENCES users(id)' +
         ')'
       ))
 
@@ -211,24 +211,79 @@ mysqlRouter.route('/insert')
 
     /* database = new Database(mysql_config); */
 
-    DATABASE.query( 'DELETE FROM listings' )
-      .then(DATABASE.query( 'DELETE FROM listing_type' ))
+    DATABASE.query( 'DELETE FROM listing_feature' )
+    .then(DATABASE.query( 'DELETE FROM features' ))
+    .then(DATABASE.query( 'DELETE FROM listings' ))
+    .then(DATABASE.query( 'DELETE FROM listing_type' ))
+    .then(DATABASE.query( 'DELETE FROM users' ))
+
+
+
       .then(DATABASE.query('INSERT INTO listing_type SET id = 1, slug = "apartment", name = "Apartment"' ))
       .then(DATABASE.query('INSERT INTO listing_type SET id = 2, slug = "bungalow", name = "Bungalow"'))
       .then(DATABASE.query('INSERT INTO listing_type SET id = 3, slug = "room", name = "Room"'))
 
-      .then(DATABASE.query('INSERT INTO listings SET ' +
-      'price = 1000.99, ' +
-      'title = "title one", ' +
-      'description = "description one", ' +
-      'address = "address one", ' +
-      'thumb = ?, ' +
-      'zipcode = 99, ' +
-      'num_bed = 2, ' +
-      'num_bath = 2, ' +
-      'size = 3, ' +
-      'score = 5, ' +
-      'listing_type_id = 1', img1
+      .then(DATABASE.query('INSERT INTO users SET '+
+      'id = 1,' +
+      'name = "admin",'       +
+      'email = "admin.1@gmail.com",' +
+      'password = "admin@123",' +
+      'phone = 4130001234,' +
+      'isblocked = 0'))
+
+      .then(DATABASE.query('INSERT INTO users SET '+
+      'id = 2,' +
+      'name = "user1",'       +
+      'email = "user.1@gmail.com",' +
+      'password = "user1@123",' +
+      'phone = 4130005678,' +
+      'isblocked = 0'))
+
+      .then(DATABASE.query('INSERT INTO users SET '+
+      'id = 3,' +
+      'name = "user2",'       +
+      'email = "user.2@gmail.com",' +
+      'password = "user2@123",' +
+      'phone = 4131111234,' +
+      'isblocked = 0'))
+
+      .then(DATABASE.query('INSERT INTO users SET '+
+      'id = 4,' +
+      'name = "user3",'       +
+      'email = "user.3@gmail.com",' +
+      'password = "user3@123",' +
+      'phone = 4131111222,' +
+      'isblocked = 0'))
+
+      .then(DATABASE.query('INSERT INTO users SET '+
+      'id = 5,' +
+      'name = "user4",'       +
+      'email = "user.4@gmail.com",' +
+      'password = "user4@123",' +
+      'phone = 4131111244,' +
+      'isblocked = 0'))
+
+      .then(DATABASE.query('INSERT INTO users SET '+
+      'id = 6,' +
+      'name = "user5",'       +
+      'email = "user.5@gmail.com",' +
+      'password = "user5@123",' +
+      'phone = 4131111211,' +
+      'isblocked = 0'))
+
+        .then(DATABASE.query('INSERT INTO listings SET ' +
+        'price = 1000.99, ' +
+        'title = "title one", ' +
+        'description = "description one", ' +
+        'address = "address one", ' +
+        'thumb = ?, ' +
+        'zipcode = 99, ' +
+        'num_bed = 2, ' +
+        'num_bath = 2, ' +
+        'size = 3, ' +
+        'score = 5, ' +
+        'user_id = 2,' +
+        'listing_type_id = 1', img1
       ))
 
       .then(DATABASE.query('INSERT INTO listings SET ' +
@@ -242,6 +297,7 @@ mysqlRouter.route('/insert')
       'num_bath = 3, ' +
       'size = 3, ' +
       'score = 4, ' +
+      'user_id = 3,' +
       'listing_type_id = 2', img2
       ))
 
@@ -256,6 +312,7 @@ mysqlRouter.route('/insert')
       'num_bath = 2, ' +
       'size = 3, ' +
       'score = 5, ' +
+      'user_id = 4,' +
       'listing_type_id = 1', img3
       ))
 
@@ -270,6 +327,7 @@ mysqlRouter.route('/insert')
       'num_bath = 2, ' +
       'size = 3, ' +
       'score = 4, ' +
+      'user_id = 5,' +
       'listing_type_id = 2', img4
       ))
 
@@ -284,6 +342,7 @@ mysqlRouter.route('/insert')
       'num_bath = 2, ' +
       'size = 3, ' +
       'score = 3, ' +
+      'user_id = 2,' +
       'listing_type_id = 1', img5
       ))
 
@@ -298,6 +357,7 @@ mysqlRouter.route('/insert')
       'num_bath = 2, ' +
       'size = 2, ' +
       'score = 5, ' +
+      'user_id = 6,' +
       'listing_type_id = 2', img6
       ))
 
@@ -313,10 +373,29 @@ mysqlRouter.route('/insert')
       'num_bath = 2, ' +
       'size = 3, ' +
       'score = 3, ' +
+      'user_id = 5,' +
       'listing_type_id = 1', img7
       ))
 
+      .then(DATABASE.query('INSERT INTO features SET id = 1, name = "Safe and secure"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 2, name = "Parking"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 3, name = "Disability Access"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 4, name = "Elevator"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 5, name = "Fitness Center"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 6, name = "Furnished"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 7, name = "Wooden Flooring"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 8, name = "Carpet Flooring"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 9, name = "Fireplace"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 10, name = "Air Contioned"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 11, name = "Pet Friendly"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 12, name = "Laundary on Site"' ))
+      .then(DATABASE.query('INSERT INTO features SET id = 13, name = "Emergency Maintainence"' ))
+
+
+
+
     .then( rows => DATABASE.query( 'SELECT id, price, title, description, address FROM listings' ) )    
+  
     .then( rows => res.send(rows) );
 
   });
