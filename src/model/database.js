c2d2042f4e1c6f72bc80ea2d2d5db7e50541667d
Cory <cjl9703@yahoo.com>
2019-04-20 00:43:13 -0700

    Pooling Added, listingRoutes.js modified to work with pooling

--- src/model/database.js
+++ src/model/database.js
@@ -5,17 +5,18 @@ Description: This file holds the pool data for connections and also
 keeps reference to the `pool` variable so the class `Database` has access to query() and also 
 pre-query tables so they can be quickly retrieved in the future to decrease query time on client side
 */
-// const mysql = require('mysql2/promise');
+ const mysql = require('mysql2/promise');
 
-// const pool = mysql.createPool({
-//     host: '18.144.46.90',
-//     user: 'team11',
-//     password: 'csc648Team11@',
-//     database: 'team11db',
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-//   });
+ const pool = mysql.createPool({
+    host: '18.144.46.90',
+    user: 'team11',
+    password: 'csc648Team11@',
+    database: 'team11db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+  module.exports = pool;
 
 //   class Database {
 //       constructor(pool) {
@@ -32,38 +33,38 @@ pre-query tables so they can be quickly retrieved in the future to decrease quer
 
 //   module.exports = new Database(pool);
 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const mysql_config = {
-    host: '18.144.46.90',
-    user: 'team11',
-    password: 'csc648Team11@',
-    port: 3306,
-    database: 'team11db',
-  };
+// const mysql_config = {
+//     host: '18.144.46.90',
+//     user: 'team11',
+//     password: 'csc648Team11@',
+//     port: 3306,
+//     database: 'team11db',
+//   };
 
-class Database {
-  constructor( config ) {
-      this.connection = mysql.createConnection( config );
-  }
-  query( sql, args ) {
-      return new Promise( ( resolve, reject ) => {
-          this.connection.query( sql, args, ( err, rows ) => {
-              if ( err )
-                  return reject( err );
-              resolve( rows );
-          } );
-      } );
-  }
-  close() {
-      return new Promise( ( resolve, reject ) => {
-          this.connection.end( err => {
-              if ( err )
-                  return reject( err );
-              resolve();
-          } );
-      } );
-  }
-}
+// class Database {
+//   constructor( config ) {
+//       this.connection = mysql.createConnection( config );
+//   }
+//   query( sql, args ) {
+//       return new Promise( ( resolve, reject ) => {
+//           this.connection.query( sql, args, ( err, rows ) => {
+//               if ( err )
+//                   return reject( err );
+//               resolve( rows );
+//           } );
+//       } );
+//   }
+//   close() {
+//       return new Promise( ( resolve, reject ) => {
+//           this.connection.end( err => {
+//               if ( err )
+//                   return reject( err );
+//               resolve();
+//           } );
+//       } );
+//   }
+// }
 
-module.exports = new Database(mysql_config);
+//module.exports = new Database(mysql_config);
